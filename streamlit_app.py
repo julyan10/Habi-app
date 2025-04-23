a5551b732a0778934a07f22bbf2fcab1b367c774
julyan10 <126521665+julyan10@users.noreply.github.com>
2025-04-23 10:21:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,7 +216,7 @@ st.subheader("💰 Precio promedio y cantidad de propiedades por ciudad")
 # Agrupamos
 df_ciudad = df_filtrado.groupby("ciudad").agg(
     Precio_Promedio=("precio", "mean"),
-    Cantidad=("ciudad", "count")
+    Cantidad=("Ciudad", "count")
 ).reset_index()
 
 # Redondeamos y preparamos para gráfico
