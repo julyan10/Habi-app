0ec2d90e091dc69322ea12725a354ce98e68a9e3
julyan10 <126521665+julyan10@users.noreply.github.com>
2025-04-23 10:59:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from math import radians, cos, sin, sqrt, atan2
 
 
 st.set_page_config(layout="wide")
-st.title("\U0001F3E0 Dashboard de propiedades - Hai Colombia / Julian Garcia")
+st.title("\U0001F3E0 Dashboard de propiedades - Habi Colombia - Julian Garcia")
 
 # --- Función para asignar ciudad basada en coordenadas ---
 def asignar_ciudad(lat, lon):
